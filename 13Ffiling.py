a0eff2ad2eea27c4b6164c589757c7f3e213e54b
Mack Delany <mackdelany@Macks-MacBook.local>
2019-02-24 18:35:20 -0500

    Readability Updates

--- 13Ffiling.py
+++ 13Ffiling.py
@@ -1,17 +1,15 @@
-### Test scrap of SEC Ark Investment Page
+### Example scrap of a 13F Ark Investment Page
+### Example URL:
 ### https://www.sec.gov/Archives/edgar/data/1697748/000114420418057760/xslForm13F_X01/infotable.xml
 
 # Importing packages
 import pandas as pd
 import numpy as np
 import re
-
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-
 import seaborn as sns
-#%matplotlib inline
 
 # Importing URL request and BeautifulSoup functions
 from urllib.request import urlopen
@@ -28,7 +26,7 @@ type(soup)
 # Checking the text
 rows = soup.find_all('tr')
 
-# Taking table rows into list form
+# Taking table rows into list 
 for row in rows:
     row_td = row.find_all('td')
 
@@ -36,7 +34,7 @@ for row in rows:
 str_cells = str(row_td)
 cleantext = BeautifulSoup(str_cells, "lxml").get_text()
 
-# finding all characters inside <td> html tags and replacing
+# Finding all characters inside <td> html tags and replacing
 # them with an empty string for each table row
 
 list_rows = []
@@ -49,20 +47,16 @@ for row in rows:
 type(clean2)
 
 # Data clean up
-df = pd.DataFrame(list_rows)
+tableOf13FFiling = pd.DataFrame(list_rows)
 
 # Removing commas
-df1 = df[0].str.split(',', expand=True)
+tableOf13FFiling = tableOf13FFiling[0].str.split(',', expand=True)
 
 # Removing square brackets in first column
-df1[0] = df1[0].str.strip('[')
+tableOf13FFiling[0] = tableOf13FFiling[0].str.strip('[')
 
 # Removing first 10 rows
-###df2 = df1.last.
+tableOf13FFiling = tableOf13FFiling.drop([0,1,2,3,4,5,6,7,8,9], axis = 0)
+tableOf13FFiling.to_csv('13FfilingExample.csv')
 
-## Need to work out how to remove last rows go back to https://www.datacamp.com/community/tutorials/web-scraping-using-python
-
-df1 = df1.drop([0,1,2,3,4,5,6,7,8,9], axis = 0)
-df1.to_csv('ARKsample.csv')
-
-print(df1)
+print(tableOf13FFiling)
