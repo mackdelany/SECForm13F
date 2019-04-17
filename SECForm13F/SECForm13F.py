f28ad4fe6b51a6606edd80e059d0b716eb9e2933
mackdelany <mackdelany@gmail.com>
2019-04-17 13:23:54 +0100

    Functionality to export multiple filings

---
+++ SECForm13F/SECForm13F.py
@@ -0,0 +1,107 @@
+### Example scrap of a 13F Ark Investment Page
+### Example URL:
+### https://www.sec.gov/Archives/edgar/data/1697748/000114420418057760/xslForm13F_X01/infotable.xml
+
+## Test
+## https://www.sec.gov/Archives/edgar/data/1697748/000114420418027944/xslForm13F_X01/infotable.xml
+## ARK CIK: 1697748
+## ARK Acc-no for 2018-05-14: 000114420418027944
+
+## import SECForm13F
+## testFirm = SECForm13F.investmentFirm("ARK Investment Management LLC", 1697748)
+## listOfFilings = testFirm.getlistOf13Ffilings()
+
+## or to return all 13F Forms as .csv's
+## testFirm.export13FfilingsAsCSVs()
+
+
+## https://www.sec.gov/cgi-bin/browse-edgar?CIK=0001697748&action=getcompany&type=13F
+
+import pandas as pd
+import numpy as np
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+class investmentFirm():
+
+    def __init__(self, companyName, centralIndexKey):
+        self.companyName = companyName
+        self.centralIndexKey = centralIndexKey
+
+    def getAccNumbersFromCIK(self):
+        url = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + self.cik + "&action=getcompany&type=13F"
+
+        html = urlopen(url)
+        pageWithAllForms = BeautifulSoup(html, 'lxml')
+        pageRows = pageWithAllForms.find_all('td')
+        accNumbers = []
+
+        for row in pageRows:
+            rowString = str(row)
+            if "<br/>Acc-no:" in rowString:
+                numbersFromRow = re.findall('\d+', rowString)
+                accNo = numbersFromRow[0] + numbersFromRow[1] + numbersFromRow[2]
+                accNumbers.append(accNo)
+
+        return accNumbers
+
+    def get13FfilingURLs(self, accNumbers):
+        URLs = []
+
+        for accNo in accNumbers:
+            url = "https://www.sec.gov/Archives/edgar/data/" + self.centralIndexKey + "/" + accNo + "/xslForm13F_X01/infotable.xml"
+            URLs.append(url)
+
+        return URLs
+
+    def getDataframeOfFiling(self, url):
+
+        html = urlopen(url)
+
+        soup = BeautifulSoup(html, 'lxml')
+        rows = soup.find_all('tr')
+
+        for row in rows:
+            row_td = row.find_all('td')
+
+        str_cells = str(row_td)
+        cleantext = BeautifulSoup(str_cells, "lxml").get_text()
+
+        list_rows = []
+        for row in rows:
+            cells = row.find_all('td')
+            str_cells = str(cells)
+            clean = re.compile('<.*?>')
+            clean2 = (re.sub(clean, '',str_cells))
+            list_rows.append(clean2)
+
+        tableOf13FFiling = pd.DataFrame(list_rows)
+        tableOf13FFiling = tableOf13FFiling[0].str.split(',', expand=True)
+        tableOf13FFiling[0] = tableOf13FFiling[0].str.strip('[')
+        tableOf13FFiling = tableOf13FFiling.drop([0,1,2,3,4,5,6,7,8,9], axis = 0)
+
+        return tableOf13FFiling
+
+
+    def getAll13Ffilings(self):
+
+        listOfFilingDataframes = []
+
+        accNumnbers = getAccNumbersFromCIK()
+        filingURLs = get13FfilingURLs(accNumnbers)
+
+        for filing in filingURLs:
+            filingDataframe = getDataframeOfFiling(filing)
+            listOfFilingDataframes.append(filingDataframe)
+
+        return listOfFilingDataframes
+
+
+    def export13FfilingsAsCSVs(self):
+        dictionaryOfFilings = getDictionaryOf13Ffilings()
+        placeholderNumber = 1
+
+        for filing in listOfFilingDataframes:
+            filing.to_csv('placeholder_filing_name' + placeholderNumber + '.csv')
+            placeholderNumber += 1
---
+++ SECForm13F/__init__.py
@@ -0,0 +1,4 @@
+from .SECForm13F import *
+import glob
+
+__version__ = "1.0"
